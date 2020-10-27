88d5b4d2ac9d28f86792ec5262e0f76279e0a3ac
Andres Mesa <acmesarodriguez@gmail.com>
2020-10-27 12:04:50 -0500

    cambio

--- apps/noc-mode.js
+++ apps/noc-mode.js
@@ -1,17 +1,17 @@
 let noche = document.getElementById("noc-mode");
-// let estilo;
-// let estiloFav;
-// let estiloCrear;
-// let estiloGifos;
+let estilo;
+let estiloFav;
+let estiloCrear;
+let estiloGifos;
 noche.addEventListener("click", (estilo) => {
     estilo = document.getElementById("estilos").href = "styles-noc.css";
-    // estiloFav = document.getElementById("estilos-fav").href = "styles-fav-noc.css";
-    // estiloCrear = document.getElementById("estilos-gifos").href = "styles-gifos-noc.css";
-    // estiloGifos = document.getElementById("estilos-crear").href = "styles-crear-noc.css";
+    estiloFav = document.getElementById("estilos-fav").href = "styles-fav-noc.css";
+    estiloCrear = document.getElementById("estilos-gifos").href = "styles-gifos-noc.css";
+    estiloGifos = document.getElementById("estilos-crear").href = "styles-crear-noc.css";
 })
 noche.addEventListener("dblclick", (estilo) => {
     estilo = document.getElementById("estilos").href = "styles.css";
-    // estiloFav = document.getElementById("estilos-fav").href = "styles-fav.css";
-    // estiloCrear = document.getElementById("estilos-gifos").href = "styles-gifos.css";
-    // estiloGifos = document.getElementById("estilos-crear").href = "styles-crear.css";
+    estiloFav = document.getElementById("estilos-fav").href = "styles-fav.css";
+    estiloCrear = document.getElementById("estilos-gifos").href = "styles-gifos.css";
+    estiloGifos = document.getElementById("estilos-crear").href = "styles-crear.css";
 })
