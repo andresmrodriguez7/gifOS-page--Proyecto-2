ca5556e90dd0c6044c850f483a33cb7177a10d0b
andresmrodriguez7 <68892764+andresmrodriguez7@users.noreply.github.com>
2020-10-27 01:10:57 -0500

    modo nocturno para todos

--- apps/noc-mode.js
+++ apps/noc-mode.js
@@ -1,10 +1,17 @@
-console.log("aqui");
 let noche = document.getElementById("noc-mode");
-console.log(noche);
-let estilo;
+// let estilo;
+// let estiloFav;
+// let estiloCrear;
+// let estiloGifos;
 noche.addEventListener("click", (estilo) => {
     estilo = document.getElementById("estilos").href = "styles-noc.css";
+    // estiloFav = document.getElementById("estilos-fav").href = "styles-fav-noc.css";
+    // estiloCrear = document.getElementById("estilos-gifos").href = "styles-gifos-noc.css";
+    // estiloGifos = document.getElementById("estilos-crear").href = "styles-crear-noc.css";
 })
 noche.addEventListener("dblclick", (estilo) => {
     estilo = document.getElementById("estilos").href = "styles.css";
+    // estiloFav = document.getElementById("estilos-fav").href = "styles-fav.css";
+    // estiloCrear = document.getElementById("estilos-gifos").href = "styles-gifos.css";
+    // estiloGifos = document.getElementById("estilos-crear").href = "styles-crear.css";
 })
