0098d8afb312db74b97142c44d86cc0c10e6c630
andresmrodriguez7 <68892764+andresmrodriguez7@users.noreply.github.com>
2020-12-13 00:27:37 -0500

    mejoras en grabacion gif

--- apps/grabar.js
+++ apps/grabar.js
@@ -1,44 +1,60 @@
 let btnEmpezar = document.getElementById("btn-comenzar");
 let video = document.getElementById("marco-video");
 let btnTerminar = document.getElementById("btn-terminar");
+let btnGrabar = document.getElementById("btn-grabar");
 let mostrarGif = document.getElementById("mostrarGif");
 let btnGuardar = document.getElementById("btn-guardar");
+let descript = document.getElementById("video-startup");
+let permisos = document.getElementById("permisos");
 let recorder = null;
-let pathUpload = "https://upload.giphy.com/v1/gifs";
 const apiKey = "3cqcb8LEg33MtM0vWp2nMTE6iMswMXML";
+let pathUpload = "https://upload.giphy.com/v1/gifs?api_key=3cqcb8LEg33MtM0vWp2nMTE6iMswMXML";
+
+let misGIFOS = [];
+let form = new FormData();
 
 
 function getStreamAndRecord() {
     navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            height: { max: 480 }
-        }
-    })
-        .then(function (stream) {
+            audio: false,
+            video: {
+                height: { max: 480 }
+            }
+        })
+        .then(function(stream) {
             video.srcObject = stream;
             video.play();
             recorder = RecordRTC(stream, {
                 type: 'gif',
-                frameRate: 7,
+                frameRate: 1,
                 quality: 10,
                 width: 370,
                 hidden: 240,
-                onGifRecordingStarted: function () {
+                onGifRecordingStarted: function() {
                     console.log('started')
                 },
             });
             recorder.startRecording();
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.log('error', err);
         });
 }
 
 btnEmpezar.addEventListener('click', () => {
+    btnGrabar.style.display = "block";
+    btnEmpezar.style.display = "none";
+    descript.style.display = "none";
+    permisos.style.display = "block";
+});
+
+btnGrabar.addEventListener('click', () => {
     getStreamAndRecord();
     btnTerminar.style.display = "block";
-    btnEmpezar.style.display = "none";
+    btnGrabar.style.display = "none";
+    descript.style.display = "none";
+    permisos.style.display = "none";
+
 });
 
 btnTerminar.addEventListener('click', () => {
@@ -51,7 +67,26 @@ btnTerminar.addEventListener('click', () => {
         mostrarGif.src = uri;
         btnTerminar.style.display = "none";
         btnGuardar.style.display = "block";
+        form.append('file', blob, 'myGif.gif');
         // form.append('file', blob, 'myGif.gif');
         // console.log(form.get('file'));
     });
 })
+
+btnGuardar.addEventListener('click', () => {
+    let idGifoCreado = createGif(form);
+    misGIFOS.push(idGifoCreado);
+    console.log(misGIFOS);
+
+});
+
+async function createGif(formdata) {
+    const response = await fetch(pathUpload, {
+        method: 'POST',
+        body: formdata,
+        mode: "no-cors"
+    });
+    console.log(response);
+    const result = await response.json();
+    return result.data.id;
+}
